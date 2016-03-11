0500df26b996733a9b5d887db44dfaa429cff3ef
unknown <jahmenev@Consultant.cbm.petrsu.ru>
2016-03-11 12:20:18 +0300

    Изменена структура проекта

--- FilteringImage.Core/ColorChannel.cs
+++ FilteringImage.Core/ColorChannel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace FilteringImage
+namespace FilteringImage.Core
 {
   public enum RGBChannel { Red, Green, Blue }
 
--- FilteringImage.Core/Noise.cs
+++ FilteringImage.Core/Noise.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace FilteringImage
+namespace FilteringImage.Core
 {
   public static class Noise
   {
