5b8413a62b207c87cddb903ef6253b83ffbbd6a5
unknown <jahmenev@Consultant.cbm.petrsu.ru>
2016-03-21 12:17:21 +0300

    Структура с результатами преобразования Фурье перенесена в отдельный файл

--- FilteringImage.Core/Fourier.cs
+++ FilteringImage.Core/Fourier.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FilteringImage.Core
 {
   public static class Fourier
   {
     private const int defaultM = 100;
+
     /*
       Прямое дискретное преобразование Фурье
 
@@ -167,26 +164,5 @@ namespace FilteringImage.Core
     {
       return x % 2 == 0;
     }
-  }
-
-  //Структура результата для преобразования Фурье
-  public struct FourierResult
-  {
-    public FourierResult(int m)
-    {
-      ReDFT = new double[m];
-      ImDFT = new double[m];
-      ReIDFT = new double[m];
-      ImIDFT = new double[m];
-      FourierSpectrum = new double[m];
-      PowerSpectrum = new double[m];
-    }
-
-    public double[] ReDFT { get; set; }           //Действительная часть прямного Фурье-преобразования
-    public double[] ImDFT { get; set; }           //Мнимая часть прямного Фурье-преобразования
-    public double[] ReIDFT { get; set; }          //Действительная часть обратного Фурье-преобразования
-    public double[] ImIDFT { get; set; }          //Мнимая часть обратного Фурье-преобразования
-    public double[] FourierSpectrum { get; set; } //Спектр Фурье преобразования
-    public double[] PowerSpectrum { get; set; }   //Спектр мощности (энергетический спектр)
   }
 }
---
+++ FilteringImage.Core/FourierResult.cs
@@ -0,0 +1,23 @@
+﻿namespace FilteringImage.Core
+{
+  //Структура результата для преобразования Фурье
+  public struct FourierResult
+  {
+    public FourierResult(int m)
+    {
+      ReDFT = new double[m];
+      ImDFT = new double[m];
+      ReIDFT = new double[m];
+      ImIDFT = new double[m];
+      FourierSpectrum = new double[m];
+      PowerSpectrum = new double[m];
+    }
+
+    public double[] ReDFT { get; set; }           //Действительная часть прямного Фурье-преобразования
+    public double[] ImDFT { get; set; }           //Мнимая часть прямного Фурье-преобразования
+    public double[] ReIDFT { get; set; }          //Действительная часть обратного Фурье-преобразования
+    public double[] ImIDFT { get; set; }          //Мнимая часть обратного Фурье-преобразования
+    public double[] FourierSpectrum { get; set; } //Спектр Фурье преобразования
+    public double[] PowerSpectrum { get; set; }   //Спектр мощности (энергетический спектр)
+  }
+}
